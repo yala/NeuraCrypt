f9fc2c0587491fc3d8eef10b20defe66498d24b4
Adam Yala <adamyala@mit.edu>
2021-06-14 17:42:51 -0400

    debug mmd loss for exported data case

--- sandstone/learn/losses/mmd.py
+++ sandstone/learn/losses/mmd.py
@@ -63,10 +63,14 @@ def get_mmd_loss(model_output, batch, model, args):
         x = batch['x'].mean(dim=1)
 
     B, C = x.size()
-    is_real = batch['source'].bool().unsqueeze(-1)
-    is_gen = ~ is_real
-    real = torch.masked_select(x, is_real).view([-1, C])
-    gen = torch.masked_select(x, is_gen).view([-1, C])
+    if args.load_data_from_encoded_dir:
+        gen = x
+        real = batch['z'].mean(dim=1) if not args.remove_pixel_shuffle else batch['z'].reshape( [batch['z'].size()[0], -1])
+    else:
+        is_real = batch['source'].bool().unsqueeze(-1)
+        is_gen = ~ is_real
+        real = torch.masked_select(x, is_real).view([-1, C])
+        gen = torch.masked_select(x, is_gen).view([-1, C])
 
     cost = torch.mean(gaussian_kernel(real, real, sigmas))
     cost += torch.mean(gaussian_kernel(gen, gen, sigmas))
